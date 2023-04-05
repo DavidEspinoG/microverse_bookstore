095206aaa21b9a52915f3c03911b45bac35be3ae
David Espino <davidespinog@gmail.com>
2023-04-04 21:00:13 -0600

    Add styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import logo from '../assets/userLogo.svg';
 
 const Header = () => (
   <header className="header">
-    <div className="header-right">
-      <h1>Book Store</h1>
-      <nav>
-        <NavLink to="/">Books</NavLink>
-        <NavLink to="/categories">Categories</NavLink>
-      </nav>
-    </div>
     <div className="header-left">
-      <img src={logo} alt="user logo" className="user-logo" />
+      <h1>Book Store CMS</h1>
+      <NavLink to="/">Books</NavLink>
+      <NavLink to="/categories">Categories</NavLink>
+    </div>
+    <div className="header-right">
+      <div className="logo-container">
+        <img src={logo} alt="user logo" className="user-logo" />
+      </div>
     </div>
   </header>
 );
